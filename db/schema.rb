a6b75b9fd4fb92ed09759bf70b91ca3b0b137a83
John Cordero <johncorderox@gmail.com>
2018-11-30 23:44:57 -0800

    user model for devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_30_010209) do
-
-  create_table "companies", force: :cascade do |t|
-    t.string "name"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_companies_on_user_id"
-  end
+ActiveRecord::Schema.define(version: 2018_11_30_090933) do
 
   create_table "items", force: :cascade do |t|
     t.string "name"
@@ -35,6 +27,18 @@ ActiveRecord::Schema.define(version: 2018_11_30_010209) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["item_id"], name: "index_menus_on_item_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
